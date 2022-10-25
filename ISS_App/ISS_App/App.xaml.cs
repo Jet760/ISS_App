cdeb8b574b55197e789ffc2997bb06c2d1196d7e
Jess <jessicarturner00@gmail.com>
2022-10-25 10:21:02 +0800

    Add project files.

---
+++ ISS_App/ISS_App/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ISS_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            NavTab n = new NavTab();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ISS_App/ISS_App/NavTab.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ISS_App
+{
+    public partial class NavTab : TabbedPage
+    {
+        // previous screen size
+        private double width = 0;
+        private double height = 0;
+        public NavTab()
+        {
+            InitializeComponent();
+            //UpdateLayout();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); // THIS MUST STAY
+
+            if(this.width != width || this.height != height)
+            {
+                this.width = width;
+                this.height = height;
+                // screen size has changed
+            }
+        }
+
+        /// <summary>
+        /// Decide on Portrait or Landscape
+        /// </summary>
+/*        private void UpdateLayout()
+        {
+            if (Content == null) return;
+            string inputString = Content.FindByName<Entry>("inout")?.Text ?? "";
+            if(width > height)
+            {
+                // landscape
+                
+                // if there is data pass in here
+                Content = new HomeLandscape(inputString);
+
+            }
+            else
+            {
+                // portrait
+                Content = new HomePortrait(inputString);
+            }
+        }*/
+    }
+}
