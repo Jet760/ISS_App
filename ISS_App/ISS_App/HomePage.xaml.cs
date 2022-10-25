0808eaa7ac1c9d9499d6bb724a353484c883e4a6
Jess <jessicarturner00@gmail.com>
2022-10-25 11:38:38 +0800

    Hooked up the API that gets latitude and longitude of the ISS to a basic page displaying the vaules

--- ISS_App/ISS_App/HomePage.xaml.cs
+++ ISS_App/ISS_App/HomePage.xaml.cs
@@ -1,20 +1,38 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace ISS_App
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        WhereTheISSAPI.Rootobject LocationAPI = new WhereTheISSAPI.Rootobject();
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        public async void GetAPI()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://api.wheretheiss.at/v1/satellites/25544");
+            var responseString = await response.Content.ReadAsStringAsync();
+            LocationAPI = JsonConvert.DeserializeObject<WhereTheISSAPI.Rootobject>(responseString);
+        }
+
+        private void buttonRefresh_Clicked(object sender, EventArgs e)
+        {
+            GetAPI();
+            labelLat.Text = LocationAPI.latitude.ToString();
+            labelLong.Text = LocationAPI.longitude.ToString();
+
         }
     }
 }
