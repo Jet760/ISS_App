931d9f10d3b03dd29edbabc398f94fec3c4c5798
Jess <jessicarturner00@gmail.com>
2022-11-07 22:07:08 +0800

    started adding code to work with files for notifs

--- ISS_App/ISS_App/Notifications/NotificationsController.cs
+++ ISS_App/ISS_App/Notifications/NotificationsController.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using PCLStorage;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ISS_App.Notifications
 {
     internal class NotificationsController
     {
+        NotificationsDataModel model = new NotificationsDataModel();
 
+        public async Task AddNotifToFileAsync(string name, string latitude, string longitude, string icon)
+        {
+            NotificationClass.Notification newNotif = new NotificationClass.Notification(name, latitude, longitude, icon);
+            await model.AddANotifToFileAsync(newNotif);
+        }
+
+        public async Task<List<NotificationClass.Notification>> GetNotifListAsync()
+        {
+            return await model.GetNotifListAsync();
+        }
     }
 }
--- ISS_App/ISS_App/Notifications/NotificationsDataModel.cs
+++ ISS_App/ISS_App/Notifications/NotificationsDataModel.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PCLStorage;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ISS_App.Notifications
 {
     internal class NotificationsDataModel
     {
+        String folderName = "ISSAppFolder";
+        String filename = "notificationsFile.txt";
 
+        public async Task AddANotifToFileAsync(NotificationClass.Notification newNotif)
+        {
+            string fileContents = await ReadFileAsync();
+
+            IFolder folder = FileSystem.Current.LocalStorage;
+            folder = await folder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+
+            List<NotificationClass.Notification> notifList = JsonConvert.DeserializeObject<List<NotificationClass.Notification>>(fileContents);
+            notifList.Add(newNotif);
+            string jsonData = JsonConvert.SerializeObject(notifList);
+
+            await file.WriteAllTextAsync(jsonData);
+        }
+
+        public async Task<string> ReadFileAsync()
+        {
+            IFolder folder = FileSystem.Current.LocalStorage;
+            folder = await folder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.GetFileAsync(filename);
+            string loadedContent = await file.ReadAllTextAsync();
+            return loadedContent;
+        }
+
+        public async Task<List<NotificationClass.Notification>> GetNotifListAsync()
+        {
+            string fileContents = await ReadFileAsync();
+            List<NotificationClass.Notification> notifList = JsonConvert.DeserializeObject<List<NotificationClass.Notification>>(fileContents);
+            return notifList;
+        }
+
+        
     }
 }
